cc19b9fd789581fb7b8837430e77618b38cc033b
Pmahdian <156665067+Pmahdian@users.noreply.github.com>
2025-11-07 14:06:20 +0330

    Docstring

--- src/scraper.py
+++ src/scraper.py
@@ -2,3 +2,9 @@ import requests
 from bs4 import BeautifulSoup  
 import pandas as pd 
 import os           
+
+def scrape_sample_data():
+    """
+    Scrape sample quotes from quotes.toscrape.com 
+    This is for practice before moving to real job sites
+    """
